a9b28eb91a9394016d04049a5f5048319f28681d
ZykinIYu <82042402+ZykinIYu@users.noreply.github.com>
2022-08-27 20:37:31 +0600

    Update README.cs

--- README.cs
+++ README.cs
@@ -124,27 +124,3 @@ driver.FindElement(By.XPath("//span[2]/button")).Clear();                     //
                 .KeyUp(Keys.Control)                                             //Отпустить кнопку Control
                 .DoubleClick()                                                   //Двойной клик кнопкой мыши
                 .Perform();                                                      //Закончить сложную операцию
-
-
-1. driver.FindElement(By.CssSelector("локатор")).Click();                        //Нажатие по элементу
-2. driver.FindElement(By.CssSelector("локатор")).SendKeys(“Текст ввода”);        //Ввод текста
-3. driver.FindElement(By.CssSelector("локатор")).Clear();                        //Очистка поля от текста
-4. new Actions(driver).moveToElement(drag).keyDown(Keys.CONTROL).clickAndHold()
-.moveToElement(drop).release().keyUp(Keys.CONTROL).perform();                    //Сложные действия
-5. driver.FindElement(By.Xpath("//*[contains(@class, 'Текст')]"));               //Локатор построенный на поиске определенного текста, можно использовать ка кпроверки
-6. Assert //проверка соответствия
-7. Alert alert = driver.switchTo().alert();
-8. Alert alert = wait.until(alertIsPresent());
-9. alert.getText();
-10. alert.sendKeys();
-11. alert.accept();
-12. alert.dismiss();
-13. caps.setCapability("unexpectedAlertBehaviour", "dismiss");
-14. driver.switchTo().frame(element)                                             //Переход во фрейм
-15. driver.switchTo().defaultContent()                                           //Переход на дефолтную страницу
-16. driver.switchTo().parentFrame()
-17. wait.until(visibilityOf(element))
-18. wait.until(visibilityOfAllElements(elementList))
-19. wait.until(not(visibilityOf(element)))
-20. wait.until(invisibilityOfAllElements(elementList))
-21. wait.until(visibilityOfElementLocated(locator))
